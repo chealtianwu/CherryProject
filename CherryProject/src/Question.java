44d14da3ed4b1f0078ccc5c9afd60b9d18a1e30a
chealtianwu <445872623@qq.com>
2018-03-27 22:41:28 +0800

    Update code
    create class Question.--chealtianwu.

---
+++ CherryProject/src/Question.java
@@ -0,0 +1,37 @@
+
+public class Question {
+	String question = new String();
+	String optionA = new String();
+	String optionB = new String();
+	String optionC = new String();
+	String optionD = new String();
+	int answer;
+	Question(String _question,String _optionA,String _optionB,
+			String _optionC,String _optionD,int _answer){
+		question = _question;
+		optionA = _optionA;
+		optionB = _optionB;
+		optionC = _optionC;
+		optionD = _optionD;
+		answer = _answer;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public String getOptionA() {
+		return optionA;
+	}
+	public String getOptionB() {
+		return optionB;
+	}
+	public String getOptionC() {
+		return optionC;
+	}
+	public String getOptionD() {
+		return optionD;
+	}
+	public int getAnswer() {
+		return answer;
+	}
+	
+}
